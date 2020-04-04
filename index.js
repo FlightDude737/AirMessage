31fcc61fe061d5a4bd9bd7f316b90d20cac68289
FlightDude737 <andrewdc@madisoncity.k12.al.us>
2020-04-04 20:40:14 +0000

    Realtime chat

--- index.js
+++ index.js
@@ -1,51 +1,23 @@
-const fs = require("fs")
-const express = require("express")
-const hbs = require('hbs')
-
-const app = express()
+const fs = require("fs");
+const app = require("express")();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const hbs = require('hbs');
 
 app.set('view engine', 'hbs')
 app.set('views', "public")
 
-app.get('/post', (req, res) => {
-  if(!req.query.content){
-    console.log("There was an error.")
-  } else {
-    const data = req.query.content;
-    const loadPosts = fs.readFileSync('posts.json')
-    const jsData = JSON.parse(loadPosts)
-    jsData.push({
-      post: data
-    })
-    const toJson = JSON.stringify(jsData)
-    fs.writeFileSync('posts.json', toJson)
-    res.send({
-      recentPost: data
-    })
-  }
-  
-})
-
-app.get('/cleardata', (req, res) => {
-  fs.writeFileSync('posts.json', '[]')
-  res.render('index.hbs')
-})
-
-app.get('/reload', (req, res) => {
-  const posts = fs.readFileSync('posts.json')
-  const allPosts = JSON.parse(posts)
-  res.send(allPosts)
+io.on('connection', (socket) => {
+  console.log('New User')
+  socket.on('send-chat-message', (message) => {
+    socket.broadcast.emit('chat-message', message)
+  })
 })
 
 app.get('/', (req, res) => {
   res.render('index.hbs')
-  fs.watchFile('posts.json', (curr, prev) => {
-    console.log("File Change Detected.")
-
-    console.log("Location Changed.")
-  })
 })
 
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log("Server is up on 3000!")
 })
